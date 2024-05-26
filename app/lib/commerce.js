2107f8f98869f64cb13507808e625d05b79d7fe2
Jeremy Bivens <bivens.jeremy@gmail.com>
2024-05-26 10:39:24 -0400

    change api

--- app/lib/commerce.js
+++ app/lib/commerce.js
@@ -1,5 +1,5 @@
 import Commerce from "@chec/commerce.js";
 
-const commerce = new Commerce(process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY);
+const commerce = new Commerce("pk_27185ef0695f9d4b480f840c8b4ceb36e07064c24a88f");
 
 export default commerce;
