63bb583ab84a77d0f3fe794c969b3de401e41f24
Jeremy Bivens <bivens.jeremy@gmail.com>
2024-05-26 12:05:55 -0400

    disabled featured

--- app/lib/commerce.js
+++ app/lib/commerce.js
@@ -1,5 +1,5 @@
 import Commerce from "@chec/commerce.js";
 
-const commerce = new Commerce("pk_27185ef0695f9d4b480f840c8b4ceb36e07064c24a88f");
+const commerce = new Commerce("pk_test_2718580e33aa0ada9f0ad04ca135858eabb947c38125b");
 
 export default commerce;
