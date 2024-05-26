44667459df98014115398dadfec509a71456837c
Jeremy Bivens <bivens.jeremy@gmail.com>
2024-05-26 12:13:13 -0400

    clean_current

--- app/components/home/Featured.jsx
+++ app/components/home/Featured.jsx
@@ -1,20 +1,19 @@
 
-// import commerce from "@/app/lib/commerce";
-// import ShopItem from "../ShopItem";
-// import Tagline from "../Tagline";
+import commerce from "@/app/lib/commerce";
+import ShopItem from "../ShopItem";
+import Tagline from "../Tagline";
 
 const Featured = async () => {
-     
-    // const { data: products } = await commerce.products.list({ limit: 32 })
-    // const dayOfTheMonth = new Date().getDate();
+    const { data: products } = await commerce.products.list({ limit: 32 })
+    const dayOfTheMonth = new Date().getDate();
     
     return (
         <section id="Featured" className="bg-foreground py-12">
-            {/* <Tagline text='Today&apos;s Must Have'/>
+            <Tagline text='Today&apos;s Must Have'/>
 
             <div className="flex bg-default rounded-md justify-center m-auto py-12 mt-6">
                 <ShopItem props={products[dayOfTheMonth]} />
-            </div> */}
+            </div>
         </section>
     );
 }
