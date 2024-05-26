6fa2e7fd67361a5b0de5a48bf0eea99057fb7f15
Jeremy Bivens <bivens.jeremy@gmail.com>
2024-05-26 09:17:09 -0400

    change commerce

--- app/lib/commerce.js
+++ app/lib/commerce.js
@@ -1,16 +1,16 @@
-import Commerce from "@chec/commerce.js";
+// import Commerce from "@chec/commerce.js";
 
 
-const publicKey = process.env.NEXT_PUBLIC_CHEC_API_KEY;
+// const publicKey = process.env.NEXT_PUBLIC_CHEC_API_KEY;
 
-const commerce = new Commerce(publicKey)
-
-export default commerce;
-
-
-
-// import CommerceSDK from "@chec/commerce.js";
-
-// const commerce = new CommerceSDK(process.env.NEXT_PUBLIC_CHEC_API_KEY);
+// const commerce = new Commerce(publicKey)
 
 // export default commerce;
+
+
+
+import CommerceSDK from "@chec/commerce.js";
+
+const commerce = new CommerceSDK(process.env.NEXT_PUBLIC_CHEC_API_KEY);
+
+export default commerce;
