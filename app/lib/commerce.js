6b79f0844a8e9b3b11cd7dbd3fca0cc8d2fbc77f
Jeremy Bivens <bivens.jeremy@gmail.com>
2024-05-26 08:42:35 -0400

    update key

--- app/lib/commerce.js
+++ app/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from "@chec/commerce.js";
 
-export const commerce = new Commerce(process.env.NEXT_PUBLIC_CHEC_API_KEY)
+export const commerce = new Commerce('pk_27185ef0695f9d4b480f840c8b4ceb36e07064c24a88f')
