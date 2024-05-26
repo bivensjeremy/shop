12bddf8f941fdc7ac21deca4f401ffaf41871de4
Jeremy Bivens <bivens.jeremy@gmail.com>
2024-05-26 09:20:31 -0400

    update api

--- app/lib/commerce.js
+++ app/lib/commerce.js
@@ -11,6 +11,6 @@
 
 import CommerceSDK from "@chec/commerce.js";
 
-const commerce = new CommerceSDK(process.env.NEXT_PUBLIC_CHEC_API_KEY);
+const commerce = new CommerceSDK('pk_27185ef0695f9d4b480f840c8b4ceb36e07064c24a88f');
 
 export default commerce;
